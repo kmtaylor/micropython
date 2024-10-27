99607658877d9429c7f63a5e5f02f9485669de5b
Kim Taylor <kmtaylor@gmx.com>
2024-10-27 21:48:48 +1100

    Remove enough features to fit into 256K of flash.

--- ports/stm32/boards/NVUM/mpconfigboard.h
+++ ports/stm32/boards/NVUM/mpconfigboard.h
@@ -1,14 +1,26 @@
 #define MICROPY_HW_BOARD_NAME       "NVUM"
 #define MICROPY_HW_MCU_NAME         "STM32F207V"
 
+#define MICROPY_QSTR_BYTES_IN_HASH  (0)
+
 #define MICROPY_HW_HAS_SWITCH       (0)
 #define MICROPY_HW_ENABLE_RTC       (0)
 // This board has an external 32kHz crystal
 #define MICROPY_HW_RTC_USE_LSE      (0)
 #define MICROPY_HW_ENABLE_SERVO     (0)
 #define MICROPY_HW_ENABLE_DAC       (0)
+#define MICROPY_HW_ENABLE_ADC       (0)
 #define MICROPY_HW_ENABLE_USB       (1)
 #define MICROPY_HW_ENABLE_RNG       (1)
+
+#define MICROPY_PY_BUILTINS_COMPLEX (0)
+#define MICROPY_PY_SOCKET           (0)
+#define MICROPY_PY_NETWORK          (0)
+#define MICROPY_PY_STM              (0)
+#define MICROPY_PY_PYB_LEGACY       (0)
+#define MICROPY_PY_HEAPQ            (0)
+#define MICROPY_PY_FRAMEBUF         (0)
+#define MICROPY_PY_PYB              (0)
 
 // HSE is 8MHz, HSI is 16MHz CPU freq set to 32MHz
 // Default source for the clock is HSI.
@@ -24,44 +36,6 @@
 #define MICROPY_HW_CLK_PLLN (336)
 #define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV4)
 #define MICROPY_HW_CLK_PLLQ (7)
-
-// UART config
-#define MICROPY_HW_UART1_TX     (pin_A9)
-#define MICROPY_HW_UART1_RX     (pin_A10)
-#define MICROPY_HW_UART2_TX     (pin_A2)
-#define MICROPY_HW_UART2_RX     (pin_A3)
-#define MICROPY_HW_UART3_TX     (pin_B10)
-#define MICROPY_HW_UART3_RX     (pin_B11)
-#define MICROPY_HW_UART4_TX     (pin_C10)
-#define MICROPY_HW_UART4_RX     (pin_C11)
-#define MICROPY_HW_UART5_TX     (pin_C12)
-#define MICROPY_HW_UART5_RX     (pin_D2)
-// UART 2 connects to the STM32F103 (STLINK) on the Nucleo board
-// and this is exposed as a USB Serial port.
-#define MICROPY_HW_UART_REPL        PYB_UART_2
-#define MICROPY_HW_UART_REPL_BAUD   115200
-
-// I2C buses
-#define MICROPY_HW_I2C1_SCL (pin_B8)        // Arduino D15, pin 3 on CN10
-#define MICROPY_HW_I2C1_SDA (pin_B9)        //         D14, pin 5 on CN10
-#define MICROPY_HW_I2C2_SCL (pin_B10)       // Arduino D6,  pin 25 on CN10
-#define MICROPY_HW_I2C2_SDA (pin_B11)       // Arduino D3,  pin 31 on CN10
-
-// SPI buses
-#define MICROPY_HW_SPI1_NSS     (pin_A15)   //              pin 17 on CN7
-#define MICROPY_HW_SPI1_SCK     (pin_A5)    // Arduino D13, pin 11 on CN10
-#define MICROPY_HW_SPI1_MISO    (pin_A6)    // Arduino D12, pin 13 on CN10
-#define MICROPY_HW_SPI1_MOSI    (pin_A7)    // Arduino D11, pin 15 on CN10
-
-#define MICROPY_HW_SPI2_NSS     (pin_B12)   //              pin 16 on CN10
-#define MICROPY_HW_SPI2_SCK     (pin_B13)   //              pin 30 on CN10
-#define MICROPY_HW_SPI2_MISO    (pin_B14)   //              pin 28 on CN10
-#define MICROPY_HW_SPI2_MOSI    (pin_B15)   //              pin 26 on CN10
-
-#define MICROPY_HW_SPI3_NSS     (pin_A4)    // Arduino A2,  pin 32 on CN7
-#define MICROPY_HW_SPI3_SCK     (pin_C10)   // Arduino D3,  pin 31 on CN10
-#define MICROPY_HW_SPI3_MISO    (pin_C11)   // Arduino D5,  pin 27 on CN10
-#define MICROPY_HW_SPI3_MOSI    (pin_C12)   // Arduino D4,  pin 29 on CN10
 
 // USRSW is pulled low. Pressing the button makes the input go high.
 #define MICROPY_HW_USRSW_PIN        (pin_C13)
--- ports/stm32/boards/NVUM/mpconfigboard.mk
+++ ports/stm32/boards/NVUM/mpconfigboard.mk
@@ -3,4 +3,13 @@ CMSIS_MCU = STM32F207xx
 AF_FILE = boards/NVUM/stm32f207_af.csv
 LD_FILES = boards/NVUM/stm32f207.ld boards/common_basic.ld
 
+# MicroPython settings
+MICROPY_VFS_FAT = 0
+MICROPY_VFS_LFS1 ?= 1
 MICROPY_HW_ENABLE_ISR_UART_FLASH_FUNCS_IN_RAM = 1
+
+# Don't include default frozen modules because MCU is tight on flash space
+FROZEN_MANIFEST ?=
+
+# LTO reduces final binary size, may be slower to build depending on gcc version and hardware
+LTO ?= 1
