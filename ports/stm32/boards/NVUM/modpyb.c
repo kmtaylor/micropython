c3c8aa1127fc305cc6a83bc5c02a2ac99e651bec
Kim Taylor <kmtaylor@gmx.com>
2024-11-09 22:25:45 +1100

    Add timer module to NVUM pyb.

--- ports/stm32/boards/NVUM/modpyb.c
+++ ports/stm32/boards/NVUM/modpyb.c
@@ -30,16 +30,18 @@
 #include "py/runtime.h"
 #include "storage.h"
 #include "usb.h"
+#include "timer.h"
 
 static const mp_rom_map_elem_t pyb_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_pyb) },
 
     { MP_ROM_QSTR(MP_QSTR_Flash), MP_ROM_PTR(&pyb_flash_type) },
 
-    { MP_ROM_QSTR(MP_QSTR_usb_mode), MP_ROM_PTR(&pyb_usb_mode_obj) },
     { MP_ROM_QSTR(MP_QSTR_USB_VCP), MP_ROM_PTR(&pyb_usb_vcp_type) },
 
     { MP_ROM_QSTR(MP_QSTR_Pin), MP_ROM_PTR(&pin_type) },
+
+    { MP_ROM_QSTR(MP_QSTR_Timer), MP_ROM_PTR(&pyb_timer_type) },
 };
 
 static MP_DEFINE_CONST_DICT(pyb_module_globals, pyb_module_globals_table);
